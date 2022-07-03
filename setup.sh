b17cc2072387e81d6ab1c7f2440c6dc6421c2294
Nova-archx86 <moorcode66@yahoo.com>
2022-07-02 23:17:31 -0700

    Removed un needed packages and refactored

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 
 function get_dotfiles() {
     git clone https://github.com/Nova-archx86/dotfiles
-    cp -r dotfiles/.config $HOME && cp -r dotfiles/.fonts $HOME
-    cp dotfiles/.bash_profile && cp dotfiles/.bashrc $HOME
-    cp .xinitrc $HOME && cp .vimrc $HOME
+    cp -r dotfiles/.config $HOME 
+    cp -r dotfiles/.fonts $HOME
+    cp dotfiles/.bash_profile
+    cp dotfiles/.bashrc $HOME
+    cp .xinitrc $HOME 
+    cp .vimrc $HOME
 }
 
 function install_packages() {
