fb3fcdc41f0c55fde45bc63cde4a18c749628d48
Nova-archx86 <moorcode66@yahoo.com>
2022-07-02 22:39:03 -0700

    inital commit

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+function get_dotfiles() {
+    git clone https://github.com/Nova-archx86/dotfiles
+    cp -r dotfiles/.config $HOME && cp -r dotfiles/.fonts $HOME
+    cp dotfiles/.bash_profile && cp dotfiles/.bashrc $HOME
+    cp .xinitrc $HOME && cp .vimrc $HOME
+}
+
+function install_packages() {
+    sudo pacman -S $(awk '{print $1}'  input_file)
+}
+
+install_packages
+get_dotfiles
