8002ff45a5b87b942deffd9059e77ab3f1ecea8b
Nova-archx86 <moorcode66@yahoo.com>
2022-07-13 14:01:50 -0700

    updated script

--- setup.sh
+++ setup.sh
@@ -1,37 +1,43 @@
-#!/bin/bash 
-
-function get_dotfiles() {
+#!/bin/sh
+clone_repos() {
     git clone https://github.com/Nova-archx86/dotfiles
+    git clone https://github.com/Nova-archx86/Scripts
+    git clone https://github.com/Nova-archx86/Dwm-build
+    git clone https://github.com/Nova-archx86/slock-build 
+    git clone https://aur.archlinux.org/paru.git
+}
+
+get_dotfiles() {
     cp -r dotfiles/.config $HOME 
     cp -r dotfiles/.fonts $HOME
     cp -r dotfiles/Wallpapers $HOME
+    cp -r Scripts $HOME
     cp dotfiles/.bash_profile $HOME
     cp dotfiles/.bashrc $HOME
     cp dotfiles/.xinitrc $HOME 
     cp dotfiles/.vimrc $HOME
 }
 
-function install_packages() {
-    sudo pacman -S $(awk '{print $1}'  packages.txt)
-    # Compiles my build of dwm 
-    git clone https://github.com/Nova-archx86/Dwm-build
+install_packages() {
     cd Dwm-build && sudo make install 
+    cd ../slock-build && sudo make install 
     cd ..
+    sudo pacman -S $(awk '{print $1}'  packages.txt)
 }
 
-function install_aur_packages() {
-    git clone https://aur.archlinux.org/paru.git
+install_aur_packages() {
     cd paru
     makepkg -si 
     cd ..
     paru -S $(awk '{print $1}' aur_pkgs.txt)
 }
 
-function install_vim_plugin_manager() {
+install_vim_plugin_manager() {
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
+clone_repos
 install_packages
 install_aur_packages
 get_dotfiles
